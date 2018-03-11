acd72e9df74ec18c522f750672635d648cb4dcb4
Brad <brad@brad.tw>
2018-03-11 12:06:29 +0800

    v2

--- app/src/main/java/tw/org/iii/mythread1/MainActivity.java
+++ app/src/main/java/tw/org/iii/mythread1/MainActivity.java
@@ -1,24 +1,56 @@
 package tw.org.iii.mythread1;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mesg;
+    private UIHandler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new UIHandler();
+
+        mesg = findViewById(R.id.mesg);
     }
 
     public void test1(View view) {
-
+        new Thread(){
+            @Override
+            public void run() {
+                for (int i=0; i<20; i++){
+                    Log.v("brad", "i = " + i);
+                    //mesg.setText("i = " + i);
+                    handler.sendEmptyMessage(i);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
     }
     public void test2(View view) {
-
     }
     public void test3(View view) {
-
     }
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            //Log.v("brad", "handler:ok");
+
+            mesg.setText("i = " + msg.what);
+        }
+    }
+
 }
