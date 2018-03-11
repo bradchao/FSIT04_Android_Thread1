7dc92ed9faadd06cd403a4b85fdac09f99c44cd2
Brad <brad@brad.tw>
2018-03-11 11:37:52 +0800

    v1

---
+++ app/src/main/java/tw/org/iii/mythread1/MainActivity.java
@@ -0,0 +1,24 @@
+package tw.org.iii.mythread1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View view) {
+
+    }
+    public void test2(View view) {
+
+    }
+    public void test3(View view) {
+
+    }
+}
